df80d73dbc19e0005161ef31f9fef2c5b7a73bb9
KevinEnriqueMartinezMartinez <127909032+KevinEnriqueMartinezMartinez@users.noreply.github.com>
2024-05-12 00:42:56 -0600

    modificacion de home
    se agrego contenido al home
    
    Co-Authored-By: SofiaRojas2890 <129335267+SofiaRojas2890@users.noreply.github.com>
    Co-Authored-By: Gerardo Marroquin <91712475+g00004@users.noreply.github.com>

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity, ScrollView } from 'react-native';
 import TopBar from '../TopBar';
 import SidebarMenu from '../SidebarMenu';
 
@@ -10,26 +10,85 @@ const HomeScreen = ({ navigation }) => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  // Mensaje de bienvenida 
+  const welcomeMessage = "¡Bienvenido al Asilo Nueva Esperanza! Donde cada día es una nueva oportunidad para crear recuerdos inolvidables.";
+
+  // Programacion de Eventos
+  const latestEvents = [
+    { title: "Concierto de Piano", date: "15 de mayo", location: "Salón Principal" },
+    { title: "Taller de Manualidades", date: "20 de mayo", location: "Sala de Actividades" },
+    { title: "Sesión de Cine", date: "25 de mayo", location: "Sala de TV" },
+    { title: "Charla sobre Nutrición", date: "30 de mayo", location: "Comedor" },
+    { title: "Bingo Nocturno", date: "5 de junio", location: "Salón de Juegos" },
+    { title: "Visita de Terapia con Mascotas", date: "10 de junio", location: "Patio Exterior" },
+    { title: "Fiesta de Cumpleaños", date: "15 de junio", location: "Salón de Fiestas" },
+    { title: "Sesión de Ejercicio Ligero", date: "20 de junio", location: "Gimnasio" },
+    { title: "Noche de Karaoke", date: "25 de junio", location: "Salón Principal" },
+    { title: "Clase de Baile", date: "30 de junio", location: "Salón de Baile" },
+    
+  ];
+
   return (
     <View style={styles.container}>
       <TopBar title="Asilo Nueva Esperanza" onPressMenu={toggleMenu} />
-      <View style={styles.content}>
-      
-      </View>
+      <ScrollView contentContainerStyle={styles.content}>
+        <Text style={styles.welcomeMessage}>{welcomeMessage}</Text>
+        <Text style={styles.sectionTitle}>Programacion de Eventos</Text>
+        {latestEvents.map((event, index) => (
+          <TouchableOpacity key={index} style={styles.eventContainer} onPress={() => alert(`Evento: ${event.title}`)}>
+            <Text style={styles.eventTitle}>{event.title}</Text>
+            <Text>{`Fecha: ${event.date} | Lugar: ${event.location}`}</Text>
+          </TouchableOpacity>
+        ))}
+        {/* Acceso rápido a Funciones Importantes */}
+        
+      </ScrollView>
       {isMenuOpen && <SidebarMenu onClose={toggleMenu} />}
     </View>
   );
 };
-//estilos
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
   },
   content: {
-    flex: 1,
-    justifyContent: 'center',
+    flexGrow: 1,
+    padding: 20,
+  },
+  welcomeMessage: {
+    fontSize: 18,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  eventContainer: {
+    backgroundColor: '#f0f0f0',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  eventTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  button: {
+    backgroundColor: '#347af0',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
     alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
